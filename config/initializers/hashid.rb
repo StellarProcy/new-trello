1517f77e3f9dade0d1ec2270b4de8bc52547e7d5
Igor 'Procy' Yukhno <procyu@gmail.com>
2020-04-27 00:17:21 +0400

    fix hashids

--- config/initializers/hashid.rb
+++ config/initializers/hashid.rb
@@ -16,4 +16,3 @@ Hashid::Rails.configure do |config|
   # Whether to sign hashids to prevent conflicts with regular IDs (see https://github.com/jcypret/hashid-rails/issues/30)
   config.sign_hashids = true
 end
-
